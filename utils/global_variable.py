d1389ec15efd3d497789ba56eb484e47580fc9e4
Matej Čušin <matej.cusin@visionect.com>
2025-09-08 18:04:00 +0200

    global variable add explanation

--- utils/global_variable.py
+++ utils/global_variable.py
@@ -1,9 +1,12 @@
-tax = 0.22
-iban = "SI56020100000012345" 
-referenca = "SI00726144"
+# Company data
 company_name = "Ta-bol štrom d.o.o."
-CSV_WAITING_DIR = "static/csv/waiting/"
-CSV_SEND_DIR = "static/csv/send/"
-plus_day_pay = 30
 reciver_address = "Ulica 1"
 reciver_post = "1000 Ljubljana"
+iban = "SI56020100000012345" 
+referenca = "SI00726144"
+# CSV files directories
+CSV_WAITING_DIR = "static/csv/waiting/"
+CSV_SEND_DIR = "static/csv/send/"
+# Other global variables
+plus_day_pay = 30 # + days from today to pay
+tax = 0.22 #tax percentage
